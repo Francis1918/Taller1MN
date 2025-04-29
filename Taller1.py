af1d8a13d84de7e674c5621de0081e0e1b035dce
Francis19 <9alemanbravo@gmail.com>
2025-04-28 21:18:38 -0500

    Codigo del Ejercicio 1V1

--- Taller1.py
+++ Taller1.py
@@ -1,1 +1,14 @@
-print('Hola Mundo')
+#Ejercicio #1
+def sumar_numeros():
+        N = int(input("Ingrese la cantidad de números (N): "))
+        SUM = 0
+        for i in range(N):
+            x = float(input(f"Ingrese x_{i+1}: "))
+            SUM += x
+        print("La suma es:", SUM)
+
+def main():
+    sumar_numeros()
+
+if __name__ == "__main__":
+    main()
